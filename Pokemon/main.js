e07a8d16c22cbed050009fd3822cc9435b6cec47
javascript-spec <72144419+javascript-spec@users.noreply.github.com>
2020-10-05 16:07:10 +0530

    Add files via upload

---
+++ Pokemon/main.js
@@ -0,0 +1,36 @@
+const pokedex = document.getElementById("pokedex");
+
+const fetchPokemon = () => {
+  const promises = [];
+  for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((result) => ({
+      name: result.name,
+      image: result.sprites["front_default"],
+      type: result.types.map((type) => type.type.name).join(", "),
+      id: result.id,
+    }));
+    displayPokemon(pokemon);
+  });
+};
+
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
+    )
+    .join("");
+  pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
