fcbb4e2beac2e3d8d2b034ff1d15214962651a50
javascript-spec <72144419+javascript-spec@users.noreply.github.com>
2020-10-12 14:50:30 +0530

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+class Customer
+   def initialize(id, name, addr)
+      @cust_id = id
+      @cust_name = name
+      @cust_addr = addr
+   end
+   def display_details()
+      puts "Customer id #@cust_id"
+      puts "Customer name #@cust_name"
+      puts "Customer address #@cust_addr"
+   end
+end
+
+# Create Objects
+cust1 = Customer.new("1", "John", "Wisdom Apartments, Ludhiya")
+cust2 = Customer.new("2", "Poul", "New Empire road, Khandala")
+
+# Call Methods
+cust1.display_details()
+cust2.display_details()
