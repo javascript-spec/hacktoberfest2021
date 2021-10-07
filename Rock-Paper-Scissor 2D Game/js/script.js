6a0bb3c52c94292e12485a933cf016a6a105f62e
TruptiSahu <truptiranjansahu866@gmail.com>
2021-10-07 13:09:14 +0530

    Added Rock Paper Scissor 2D Game

---
+++ Rock-Paper-Scissor 2D Game/js/script.js
@@ -0,0 +1,8 @@
+//* Selecting element from DOM
+const playerBtns = document.querySelectorAll('.player-btn');
+const gameRound = document.querySelector('.game__round');
+const handImg = document.querySelectorAll('.hand-img');
+const gameScorePlayer = document.querySelector('.game__score--player p');
+const gameScoreTie = document.querySelector('.game__score--draw p');
+const gameScoreComputer = document.querySelector('.game__score--computer p');
+const scoreLog = document.querySelectorAll('.scoreLog');
